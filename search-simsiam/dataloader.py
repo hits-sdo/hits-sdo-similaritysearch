290033d93b2a720c18572977f092711ec7e710df
subhamoysgit <sc8473@gmail.com>
2023-05-10 21:13:04 -0600

    two different augmentations from same source image
    Co-authored-by: Jasper <jasperdoan@users.noreply.github.com>
    Co-authored-by: max9001 <max9001@users.noreply.github.com>
    Co-authored-by: Matin Qurbanzadeh <MatinQurban@users.noreply.github.com>
    Co-authored-by: ThomasP677 <ThomasP677@users.noreply.github.com>
    Co-authored-by: Cameron Wolff <camwolff02@users.noreply.github.com>
    Co-authored-by: agomez225 <agomez225@users.noreply.github.com>

---
+++ search-simsiam/dataloader.py
@@ -0,0 +1,40 @@
+from sdo_augmentation.augmentation import Augmentations
+from utils.image_utils import read_image, stitch_adj_imgs
+import sys
+import glob
+import json
+import matplotlib.pyplot as plt
+
+sys.path.append('./')
+
+file_dir = "./data/miniset/AIA171/monochrome/"
+
+file_list = glob.glob(file_dir + "*.p")
+
+img = read_image(file_list[60], 'p')
+data_js = json.load(open('./data/Augmentationfile_EUV.json'))
+
+print(data_js.keys())
+
+a1 = Augmentations(img, data_js["Augmentation_1"][0])
+a2 = Augmentations(img, data_js["Augmentation_2"][0])
+
+aug_img1, _ = a1.perform_augmentations()
+aug_img2, _ = a2.perform_augmentations()
+
+size_tuple_img = (1, 3)
+
+plt.subplot(*size_tuple_img, 1)
+plt.imshow(img)
+
+plt.subplot(*size_tuple_img, 2)
+plt.imshow(aug_img1)
+
+plt.subplot(*size_tuple_img, 3)
+plt.imshow(aug_img2)
+plt.show()
+
+
+file_names = [x[len(file_dir):] for x in file_list]
+
+print(file_names)
