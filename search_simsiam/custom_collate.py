58f41c3840704c1a8284e812ae5ac54c0eb94308
subhamoysgit <sc8473@gmail.com>
2023-06-01 19:01:06 -0600

    argument is now list of tuples

--- search_simsiam/custom_collate.py
+++ search_simsiam/custom_collate.py
@@ -1,5 +1,8 @@
 from sdo_augmentation.augmentation_list import AugmentationList
 from sdo_augmentation.augmentation import Augmentations
+import numpy as np
+import torch
+
 
 def sunbird_collate(source_tuple):
     '''
@@ -9,30 +12,26 @@ def sunbird_collate(source_tuple):
             filename          : str
     DESC: Applies random augmentation to the images then returns them for comparsion
     '''
-    label = source_tuple[1]
-    file_name = source_tuple[2]
-
-    source_image = np.array(source_tuple[0])/255.
-    
-    print(source_image.shape)
+    imgs_t0, imgs_t1, file_names, labels = [], [], [], []
 
     A = AugmentationList(instrument="euv")
-    dict0 = A.randomize()
-    dict1 = A.randomize()
-
-    print(dict0, "\n", dict1)
-
-    Aug0 = Augmentations(source_image, dict0)
-    Aug1 = Augmentations(source_image, dict1)
-
 
     fill_type = 'Nearest'
-    img_t0, _ = Aug0.perform_augmentations(fill_void=fill_type)
-    img_t1, _ = Aug1.perform_augmentations(fill_void=fill_type)
-    
-    # t0 = torch.from_numpy(img_t0)
-    # t1 = torch.from_numpy(img_t1)
 
+    for item in source_tuple:
+        file_names.append(item[2])
+        labels.append(item[1])
+        source_image = np.array(item[0]).astype(float)/255
+        dict0 = A.randomize()
+        dict1 = A.randomize()
+        Aug0 = Augmentations(source_image, dict0)
+        Aug1 = Augmentations(source_image, dict1)
+        img_t0, _ = Aug0.perform_augmentations(fill_void=fill_type)
+        img_t1, _ = Aug1.perform_augmentations(fill_void=fill_type)
+        imgs_t0.append(img_t0)
+        imgs_t1.append(img_t1)
 
-
-    return (img_t0,img_t1),label,file_name
+    imgs_t0 = torch.from_numpy(np.array(imgs_t0)).permute(0,3,1,2)
+    imgs_t1 = torch.from_numpy(np.array(imgs_t1)).permute(0,3,1,2)
+    labels = torch.tensor(labels)
+    return (imgs_t0, imgs_t1), labels, file_names
