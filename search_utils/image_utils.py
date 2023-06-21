0edcf28c19bcdd6b42f352b9dc2da5dbca3068da
subhamoysgit <sc8473@gmail.com>
2023-06-21 17:45:57 -0600

    developed iterative superimage void filling

--- search_utils/image_utils.py
+++ search_utils/image_utils.py
@@ -1,6 +1,12 @@
 import pickle
 from PIL import Image
 import numpy as np
+from lightly.data import LightlyDataset
+import glob
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+from sdo_augmentation.augmentation import Augmentations
+import cv2
 
 
 def read_image(image_loc, image_format):
@@ -30,7 +36,41 @@ def read_image(image_loc, image_format):
 
     return image
 
-def stitch_adj_imgs(data_dir, file_name, EXISTING_FILES, multi_wl=False):
+
+def interpolate_superimage(img, loc):
+    image_top_left = (img.shape[0]*loc[0]//3, img.shape[1]*loc[1]//3)
+    image_bottom_right = (img.shape[0]*(loc[0] +1)//3, img.shape[1]*(loc[1] +1)//3)
+
+    y_lower = 0 if image_top_left[0]-1<0 else image_top_left[0] - 1
+    x_lower = 0 if image_top_left[1]-1<0 else image_top_left[1] - 1
+    y_upper = img.shape[0] -1 if image_bottom_right[0]==img.shape[0] else image_bottom_right[0] + 1
+    x_upper = img.shape[1] -1 if image_bottom_right[1]==img.shape[1] else image_bottom_right[1] + 1
+    m = np.median(img[128:256,128:256],axis=(0,1))
+    n = 1.0
+    for i in range(img.shape[0]//3):
+        for j in range(img.shape[1]//3):
+            y = image_top_left[0] + i
+            x = image_top_left[1] + j
+            # d1 = (y_upper - y)
+            # d2 = (y - y_lower)
+            # d3 = (x_upper - x)
+            # d4 = (x - x_lower)
+            d1 = 1/(0.001 + y - y_lower)**n
+            d2 = 1/(0.001 + y_upper - y)**n
+            d3 = 1/(0.001 + x - x_lower)**n
+            d4 = 1/(0.001 + x_upper - x)**n
+            I1 = img[y_lower, x] if y_lower != 0 else m
+            I2 = img[y_upper, x] if y_upper != img.shape[0] -1 else m 
+            I3 = img[y, x_lower] if x_lower != 0 else m
+            I4 = img[y, x_upper] if x_upper != img.shape[1] -1 else m
+
+            img[y,x] = (I1*d1 + I2*d2 + I3*d3 + I4*d4)/(d1 + d2 + d3 + d4)
+    
+    return img
+    
+
+
+def stitch_adj_imgs(data_dir, file_name, EXISTING_FILES, multi_wl=False, iterative = False):
     '''
     Stitches surrounding 8 images to inputted image in 
     order to not have unfilled edges 
@@ -42,6 +82,11 @@ def stitch_adj_imgs(data_dir, file_name, EXISTING_FILES, multi_wl=False):
             file name of image to find neighbors too (?)
         EXISTING_FILES(str):
             list of filenames inside data_dir
+        multi_wl (bin):
+            set as True when multi-wavelength images are used
+        iterative (bin):
+            set as True to iteratively fill blank spaces in superimage
+
 
     Returns:
         super_Image(np array)    
@@ -69,11 +114,23 @@ def stitch_adj_imgs(data_dir, file_name, EXISTING_FILES, multi_wl=False):
 
     list_info_constant = '_'.join(list_info[:-2])
 
-    coordinates = [
-        (0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
+    # coordinates = [(1,1)]
+    # removed_coords = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2)]
 
-    image_len = read_image(data_dir + file_name, file_format).shape[0]
+    # coordinates = [(0, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
+
+    # removed_coords = [(1,0), (0, 1), (0, 2)]
+
+    coordinates = [(0, 0), (0, 1), (0, 2), (1,0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
+    source_image = read_image(data_dir + file_name, file_format)
+    image_len = source_image.shape[0]
     superImage = np.zeros((3*image_len, 3*image_len, 3))
+    # superImage = np.pad(source_image, ((image_len, image_len),
+    #                                    (image_len, image_len), (0, 0)),
+    #                                    'edge')
+
+    removed_coords = []
+
     for i, j in coordinates:
         i_s = iStart - image_len + i * image_len
         j_s = jStart - image_len + j * image_len
@@ -90,5 +147,72 @@ def stitch_adj_imgs(data_dir, file_name, EXISTING_FILES, multi_wl=False):
             im = read_image(data_dir + tile_name, file_format)
             superImage[i*image_len: (i+1)*image_len, j*image_len:
                        (j+1)*image_len] = im
+        else:
+            removed_coords.append((i, j))
+    
+    if len(removed_coords)>0 and iterative==True:
+        l = []
+        for k in range(20):
+            s0 = superImage.copy()
+            count = 0      
+            for loc in removed_coords:
+                plt.imshow(superImage)
+                plt.title(f'iteration {k+1}')
+                plt.savefig('/home/schatterjee/Desktop/superImage_animation/padding_'+str(k).zfill(2)+'_'+
+                            str(count)+'.png')
+                # y0 = image_len*loc[0]
+                # y1 = image_len*(1+loc[0])
+                # x0 = image_len*loc[1]
+                # x1 = image_len*(1+loc[1])
+                # blurred = cv2.blur(superImage,(32,32))
+                # superImage[y0:y1, x0:x1] = blurred[y0:y1, x0:x1]
+                
+                superImage = interpolate_superimage(superImage, loc)
+                count += 1
+            s1 = superImage
+            delta = 100*np.sum((s1-s0)**2)/np.sum(s0**2)
+            l.append(delta)
+            if delta<10**-4:
+                break
+
 
     return superImage
+
+if __name__ == '__main__':
+    idx = 10
+    path_to_data = '/home/schatterjee/Documents/hits/aia_171_color_1perMonth'
+    dataset = LightlyDataset(input_dir=path_to_data)
+    item = dataset[idx]
+    print(item)
+    source_image = np.array(item[0]).astype(float)/255
+    path_list = item[2].split('/')
+    file_dir = path_to_data+'/'+'/'.join(path_list[:2]) +'/'
+    files_list = glob.glob(file_dir+"/*.jpg", recursive = True)
+    file_names = [x[len(file_dir):] for x in files_list]
+    file_name = path_list[2]
+
+    super_image = stitch_adj_imgs(data_dir=file_dir,
+                                  file_name=file_name,
+                                  EXISTING_FILES=file_names,
+                                  multi_wl = False)
+    
+    a = Augmentations(super_image,dct={'translate':(30,30),'rotate':15})
+    img_t, _ = a.perform_augmentations()
+    plt.subplot(1,3,1)
+    plt.imshow(source_image)
+    plt.title('Original')
+    plt.subplot(1,3,2)
+    plt.imshow(super_image)
+    plt.gca().add_patch(Rectangle((128,128),128,128,
+                    edgecolor='green',
+                    facecolor='none',
+                    lw=2))
+    plt.title('Padding 7')
+    plt.subplot(1,3,3)
+    plt.imshow(img_t[128:256,128:256])
+    plt.title('Augmented')
+
+    # plt.plot(l)
+    # plt.yscale('log')
+    # print(np.max(l))
+    plt.show()
