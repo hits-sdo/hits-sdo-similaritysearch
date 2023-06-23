c43e44e491ee94b248e24a1022f6f5fc042f9711
subhamoysgit <sc8473@gmail.com>
2023-06-23 17:50:01 -0600

    diverse_sampler on circles data
    Co-authored-by: Jasper <jasperdoan@users.noreply.github.com>
    Co-authored-by: Nicholas King <Starfractor@users.noreply.github.com>
    Co-authored-by: ThomasP677 <ThomasP677@users.noreply.github.com>
    Co-authored-by: max9001 <max9001@users.noreply.github.com>
    Co-authored-by: Matin Qurbanzadeh <MatinQurban@users.noreply.github.com>

--- search_simsiam/diverse_sampling.py
+++ search_simsiam/diverse_sampling.py
@@ -21,6 +21,7 @@ return result
 
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 
 def diverse_sampler(embedder, data, n):
@@ -36,7 +37,7 @@ def diverse_sampler(embedder, data, n):
         result (list): list of n points sampled from the embedding space
     """
     features = embedder(data)
-    result = [[0,0]]
+    result = [random.choice(data)]
     distances = [1000000] * data.shape[0]
     for _ in range(n):
         for i in range(features.shape[0]):
@@ -45,32 +46,116 @@ def diverse_sampler(embedder, data, n):
                 distances[i] = dist
         idx = distances.index(max(distances))
         result.append(features[idx])
+        # del data[idx]
+        # result.append(features[idx])
         features = np.delete(features, idx, axis=0)
-        distances = [1000000] * features.shape[0]
+        del distances[idx]
+        # distances = [1000000] * features.shape[0]
 
-    return result``
+    return result[:n]
 
 
 
 def main():
-    cov = np.array([[6, -3], [-3, 3.5]])
-    # np.random.seed(7099872)
-    data = np.random.multivariate_normal([0, 0], cov, size=800)
-    data = np.array(data)
+
+
     def embedder(x): return x
-    r = diverse_sampler(embedder, data, 600)
 
-    # test plotting TODO remove
-    # print(len(r))
-    # print(r)
+
+    numb_points_per_circle = []
+    for i in range(10):
+        if i % 2 != 1:
+            numb_points_per_circle.append(100)
+        else:
+            numb_points_per_circle.append(800)
+        
+    points = []
+    for i in range(10):
+        radius = 0.05 * (i + 1)
+
+        theta = np.random.uniform(0, 2 * np.pi, numb_points_per_circle[i])
+
+        x = 0.5 + radius * np.cos(theta)
+        y = 0.5 + radius * np.sin(theta)
+
+        circle_points = np.column_stack((x, y))
+        points.append(circle_points)
+    
+    points_array = np.vstack(points)
+
+    x = points_array[:, 0]
+    y = points_array[:, 1]
+
+
+    plt.subplot(1, 3, 1)
+    plt.scatter(x, y)
+    plt.title("Original Biased Circles Dataset")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+
+
+    np.random.shuffle(points_array)
+    random_subset_points_array = points_array[:500]
+    x = random_subset_points_array[:, 0]
+    y = random_subset_points_array[:, 1]
+    plt.subplot(1, 3, 2)
+    plt.scatter(x, y)
+    plt.title("Random Sampling")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+
+
+    points_array = np.array(points_array)
+    r = diverse_sampler(embedder, points_array, 500)
     r = np.array(r)
-    plt.subplot(1, 2, 1)
-    plt.plot(data[:, 0], data[:, 1], '.')
-    plt.subplot(1, 2, 2)
-    plt.plot(r[:, 0], r[:, 1],'.')
+    x = r[:, 0]
+    y = r[:, 1]
 
-    # plt.plot(r[:, 0], r[:, 1], '.')
+    plt.subplot(1, 3, 3)
+    plt.scatter(x, y)
+    plt.title("Diverse Sampling")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+
     plt.show()
+
+
+
+# Please use this 🥺 instead of that mess ^
+# points_array = np.vstack(points)
+# random_subset_points_array = points_array[:500]
+# def embedder(x): return x
+# r = diverse_sampling(embedder, points_array, 500)
+
+
+# title = ['Original Biased Circles Dataset', 'Random Sampling', 'Diverse Sampling']
+# ar = [points_array, random_subset_points_array, r]
+
+# fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+# for t, a, ax in zip(title, ar, axs):
+#     ax.scatter(a[:, 0], a[:, 1], s=1)
+#     ax.set_title(t)
+# plt.show()
+
+
+
+    # cov = np.array([[6, -3], [-3, 3.5]])
+    # np.random.seed(7099872)
+    # data = np.random.multivariate_normal([0, 0], cov, size=800)
+    # data = np.array(data)
+    
+    # r = diverse_sampler(embedder, data, 100)
+
+    # # test plotting TODO remove
+    # # print(len(r))
+    # # print(r)
+    # r = np.array(r)
+    # plt.subplot(1, 2, 1)
+    # plt.plot(data[:, 0], data[:, 1], '.')
+    # plt.subplot(1, 2, 2)
+    # plt.plot(r[:, 0], r[:, 1],'.')
+
+    # plt.show()
 
 
 if __name__ == '__main__':
