35182f76eea2295325d3b9714b42dfbcc83c3241
kierav <kiera.vds@gmail.com>
2023-06-28 08:41:41 -0600

    assemble embeddings

--- src/assembler.py
+++ src/assembler.py
@@ -2,6 +2,7 @@ import glob
 import pandas as pd
 import numpy as np
 import os
+from sklearn.decomposition import PCA
 
 class Assembler():
     """
@@ -9,17 +10,30 @@ class Assembler():
     tile_rows x tile_cols x embedding_dim
     """
     def __init__(self,tile_dim:int=128,img_dim:int=2048,embedding_dim:int=16,
-                 data_path:str='data',run:str=''):
+                 data_path:str='data',run:str='',datasets:list=['train','val']):
         self.tile_dim = tile_dim
         self.img_dim = img_dim
         self.embedding_dim = embedding_dim
         self.data_path = data_path
+        if not os.path.exists(self.data_path):
+            os.makedirs(self.data_path)
         self.run = run
-        self.files = sorted(glob.glob('wandb/*'+run+'/files/filenames*.csv'))
+
+        self.files = []
+        self.embedding_files = []
+        self.embeddings_proj_files = []
+        for dataset in datasets:
+            self.files.append(sorted(glob.glob('wandb/*'+run+'/files/filenames'+dataset+'.csv'))[-1])
+            self.embedding_files.append(sorted(glob.glob('wandb/*'+run+'/files/embeddings'+dataset+'.npy'))[-1])
+            self.embeddings_proj_files.append(sorted(glob.glob('wandb/*'+run+'/files/embeddings_proj'+dataset+'.npy'))[-1])
+        
+        self.df = pd.DataFrame()
+        self.embeddings = []
+        self.embeddings_proj = []
 
     def assemble_tiles(self,files,embeddings):
         """
-        Take tile parent folder and assemble tiles into an "image" of size 
+        Take tile files and assembles into an "image" of size 
         tile_rows x tile_cols x embedding_dim
 
         Parameters:
@@ -35,10 +49,90 @@ class Assembler():
         
         for file,embedding in zip(files,embeddings):
             file_split = file.split('/')[-1].strip('.npy').split('_')
-            ind_row = int(file_split[1])//128
-            ind_col = int(file_split[2])//128
+            ind_row = int(file_split[1])//self.tile_dim
+            ind_col = int(file_split[2])//self.tile_dim
             img[ind_row,ind_col,:] = embedding
                     
         return img
+    
+    def reshape_embedding(self,embeddings,reducer,fit=True):
+        """
+        Reshape embedding to specified dimension either by some dimensionality reducer
+        or by padding
+        
+        Parameters:
+            embeddings (array):     embeddings to be reshaped
+            reducer (sklearn like): has a fit_transform and a transform method
+            fit (bool):             whether or not to fit the reducer
 
+        Returns:
+            embeddings (array):     reshaped to n samples x embedding dim 
+        """
+        if np.shape(embeddings)[1] > self.embedding_dim and fit:
+            embeddings = reducer.fit_transform(embeddings)
+        elif np.shape(embeddings)[1] > self.embedding_dim and not fit:
+            embeddings = reducer.transform(embeddings)
+        else:
+            embeddings = np.pad(embeddings,((0,0),(0,self.embedding_dim-np.shape(embeddings)[1])))
+        return embeddings
 
+    def create_df(self):
+        """
+        Iterate through files and embeddings and compile into one dataframe/list
+        """
+        pca = PCA(n_components=self.embedding_dim)
+        pca_proj = PCA(n_components=self.embedding_dim)
+
+        for files,embeddings_file,embeddings_proj_file in zip(self.files,self.embedding_files,self.embeddings_proj_files):
+            file_df = pd.read_csv(files)
+            file_df['parent'] = file_df['filename'].str.split('/').str[-3]
+            file_df['sample_time'] = file_df['filename'].str.split('/').str[-3].str.rsplit('_',n=1).str[0]
+
+            embeddings = np.load(embeddings_file)
+            embeddings_proj = np.load(embeddings_proj_file)
+
+            if len(self.df)==0:
+                self.df = file_df
+                embeddings = self.reshape_embedding(embeddings,pca,fit=True)
+                embeddings_proj = self.reshape_embedding(embeddings_proj,pca_proj,fit=True)
+                self.embeddings = embeddings
+                self.embeddings_proj = embeddings_proj
+
+            else:
+                self.df = pd.concat((self.df,file_df))
+                embeddings = self.reshape_embedding(embeddings,pca,fit=False)
+                embeddings_proj = self.reshape_embedding(embeddings_proj,pca_proj,fit=False)
+                self.embeddings = np.concatenate((self.embeddings,embeddings),axis=0)
+                self.embeddings_proj = np.concatenate((self.embeddings_proj,embeddings_proj),axis=0)
+
+        self.df = self.df.reset_index(drop=True)
+
+    def assemble_all(self):
+        """
+        To be run after create_df
+        Assembles all tile embeddings, saves to data path and creates new index df
+        """
+
+        if len(self.df) == 0:
+            print('Error, must run create_df first')
+            return
+        
+        for parent in pd.unique(self.df['parent']):
+            inds = self.df['parent']==parent
+            files = self.df[inds]['filename']
+            embeddings = self.embeddings[inds]
+            embeddings_proj = self.embeddings_proj[inds]
+
+            img = self.assemble_tiles(files,embeddings)
+            img_proj = self.assemble_tiles(files,embeddings_proj)
+
+            # save image
+            np.save(self.data_path+os.sep+parent+'_embedding.npy',img)
+            np.save(self.data_path+os.sep+parent+'_embedding_proj.npy',img_proj)
+
+            self.df.loc[inds,'embedding_file'] = self.data_path+os.sep+parent+'_embedding.npy'
+            self.df.loc[inds,'embedding_proj_file'] = self.data_path+os.sep+parent+'_embedding_proj.npy'
+
+        print('Assembled',len(self.df),'tiles')
+        self.df = self.df.drop_duplicates(subset='parent')
+        print('Into',len(self.df),'embedded parent files')
--- src/tests/test_assembler.py
+++ src/tests/test_assembler.py
@@ -26,14 +26,18 @@ class AssemblerTest(unittest.TestCase):
         self.embedding_dim = 16
         self.tile_files = np.array(pd.read_csv(sorted(glob.glob('wandb/*'+run+'/files/filenamestrain.csv'))[-1])['filename'])
         self.embeddings = np.load(sorted(glob.glob('wandb/*'+run+'/files/embeddingstrain.npy'))[-1])
+        self.data_path = 'data/test_assemble'
         self.assembler = Assembler(embedding_dim=self.embedding_dim,
-                                   run=run)
+                                   run=run,data_path=self.data_path)
+        
 
     def test_assemblerExists(self):
         self.assertIsInstance(self.assembler.tile_dim,int)
         self.assertIsInstance(self.assembler.run,str)
         self.assertIsInstance(self.assembler.data_path,str)
         self.assertIsInstance(self.assembler.files,list)
+        self.assertEqual(len(self.assembler.files),len(self.assembler.embedding_files))
+        self.assertEqual(len(self.assembler.files),len(self.assembler.embeddings_proj_files))
 
     def test_assembleTiles(self):
         pca = PCA(n_components=self.embedding_dim)
@@ -41,6 +45,22 @@ class AssemblerTest(unittest.TestCase):
         img = self.assembler.assemble_tiles(self.tile_files,embeddings_pca)
         self.assertEqual(np.shape(img),(16,16,self.embedding_dim))
         self.assertGreater(np.sum(abs(img[:])),0)
+
+    def test_createDf(self):
+        self.assembler.create_df()
+        self.assertIsInstance(self.assembler.df,pd.DataFrame)
+        self.assertGreater(len(self.assembler.df),1)
+        self.assertIsInstance(self.assembler.df['parent'].iloc[0],str)
+        self.assertEqual(len(self.assembler.df),len(self.assembler.embeddings))
+
+    def test_assembleAll(self):
+        self.assembler.create_df()
+        self.assembler.assemble_all()
+        files = os.listdir(self.data_path)
+        assembled_file = np.load(self.data_path+os.sep+files[0])
+        self.assertEqual(np.shape(assembled_file),(16,16,self.embedding_dim))
+        self.assertGreater(np.sum(abs(assembled_file[:])),0)
+        self.assertEqual(len(os.listdir(self.data_path)),2*len(pd.unique(self.assembler.df['parent'])))
 
 if __name__ == "__main__":
     unittest.main()
