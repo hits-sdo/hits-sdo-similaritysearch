0b285753a4ad28da67bc9d484ebcf5d686c2e279
subhamoysgit <sc8473@gmail.com>
2023-06-28 20:38:40 -0600

    added filenames as argument to diverse_sampler
    Co-authored-by: Matin Qurbanzadeh <MatinQurban@users.noreply.github.com>
    Co-authored-by: Cameron Wolff <camwolff02@users.noreply.github.com>
    Co-authored-by: ThomasP677 <ThomasP677@users.noreply.github.com>
    Co-authored-by: max9001 <max9001@users.noreply.github.com>
    Co-authored-by: Jasper <jasperdoan@users.noreply.github.com>

--- search_simsiam/diverse_sampling.py
+++ search_simsiam/diverse_sampling.py
@@ -3,23 +3,29 @@ import matplotlib.pyplot as plt
 import random
 from sklearn.utils import shuffle
 
-def diverse_sampler(embedder, data, n):
+
+def diverse_sampler(embedder, filenames, data, n):
     """
     Parameters:
         embedder (function): embeds a high dimensional vector to a lower dim
+        filenames(list): filename
         data (list): data to embed
         n (int): number of points to sample from the embedding space
 
     Returns:
+
         result (list): list of n points sampled from the embedding space
 
     Ref:
         https://arxiv.org/pdf/2107.03227.pdf
 
     """
+    filenames_ = filenames.copy()    
     features = embedder(data)
     result = [random.choice(data)]
-    distances = [1000000] * data.shape[0]
+    filenames_results = [random.choice(filenames_)]
+    distances = [1000000] * len(data)
+    
     for _ in range(n):
         for i in range(features.shape[0]):
             dist = np.linalg.norm(features[i] - result[-1])
@@ -27,13 +33,13 @@ def diverse_sampler(embedder, data, n):
                 distances[i] = dist
         idx = distances.index(max(distances))
         result.append(features[idx])
-        # del data[idx]
-        # result.append(features[idx])
+        filenames_results.append(filenames_[idx])
+        
         features = np.delete(features, idx, axis=0)
+        del filenames_[idx]
         del distances[idx]
-        # distances = [1000000] * features.shape[0]
 
-    return np.array(result[1:])
+    return filenames_results[1:], np.array(result[1:])
 
 
 def circles_dataset():
@@ -72,10 +78,22 @@ def circles_dataset():
 def main():
     def embedder(x): return x
     points_array, colors = circles_dataset()
+
+    file_name_list = [f'file{i}' for i in range(len(points_array))]
+
     points_array, colors = shuffle(points_array, colors)
     random_subset_points_array = points_array[:500]
     colors_random = colors[:500]
-    diverse_subset_points_array = diverse_sampler(embedder, points_array, 500)
+    # diverse_subset_points_array = diverse_sampler(embedder, points_array, 500)
+    diverse_filename, diverse_subset_points_array = diverse_sampler(embedder, file_name_list, points_array, 500)
+
+    print(len(diverse_filename))
+    print(len(diverse_subset_points_array))
+
+    idx = file_name_list.index(diverse_filename[0])
+    print(points_array[idx], diverse_subset_points_array[0])
+
+
     colors_diverse = [colors[np.where(np.all(points_array == i,
                                              axis=1))[0][0]]
                       for i in list(diverse_subset_points_array)]
